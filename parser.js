3ea0b1e6023082fa60a5cac1a39cc52143e0bc82
snoopso <real@snoop.so>
2023-01-27 21:33:11 +0900

    Remove useless files

--- parser.js
+++ parser.js
@@ -1,4 +1,5 @@
 import { LexerAnalyer } from './tokenizer.js';
+import { TOKENS } from './constants.js';
 
 class Stack {
   constructor() {
@@ -31,37 +32,16 @@ class Stack {
   searchCloseTag(element) {
     let result = false;
     this.arr.forEach((v) => {
-      // console.log(v);
       if (v.element === element) result = true;
     });
     return result;
   }
 }
 
-const Type = {
-  startTagOpen: 'startTagOpen',
-  endTagOpen: 'endTagOpen',
-  startTagElement: 'startTagElement',
-  endTagElement: 'endTagElement',
-  tagClose: 'tagClose',
-  attributeKey: 'attributeKey',
-  attributeValue: 'attributeValue',
-  text: 'text',
-};
-
 const Position = {
   start: 1,
   end: 2,
 };
-
-class Tag {
-  constructor() {
-    this.position = null;
-    this.element = null;
-    this.attributes = [];
-    this.isOpened = false;
-  }
-}
 
 class Text {
   constructor(lexeme) {
@@ -96,6 +76,16 @@ class Node {
   }
 }
 
+class Tag {
+  constructor() {
+    this.position = null;
+    this.element = null;
+    this.attributes = null;
+    this.isOpened = false;
+    this.type = null;
+  }
+}
+
 export class Parser {
   constructor() {
     this.stack = new Stack();
@@ -103,156 +93,98 @@ export class Parser {
     this.currentTag = null;
   }
 
-  checkType({ lastLexeme, lexeme, nextLexeme }) {
-    let type = null;
-    switch (lexeme) {
-      case 'LeftArrowBracker':
-        type = nextLexeme !== 'Div' ? Type.startTagOpen : Type.endTagOpen;
-        break;
-      case 'RightArrowBracker':
-        type = Type.tagClose;
-        break;
-      case 'Div':
-        break;
-      case 'Equal':
-        break;
-      default:
-        switch (lastLexeme) {
-          case 'Div':
-            type = Type.endTagElement;
-            break;
-          case 'LeftArrowBracker':
-            type = Type.startTagElement;
-            break;
-          case 'RightArrowBracker':
-            if (nextLexeme === 'LeftArrowBracker') {
-              type = Type.text;
-              break;
-            }
-          case 'Equal':
-            type = Type.attributeValue;
-            break;
-          default:
-            switch (nextLexeme) {
-              case 'Equal':
-                type = Type.attributeKey;
-                break;
-            }
-        }
-    }
-    return type;
-  }
-
-  tagFactory(type, { lastLexeme, lexeme }) {
-    if (this.currentTag === null) {
-      this.currentTag = new Tag();
-    }
-
-    switch (type) {
-      case Type.startTagOpen:
-        this.currentTag.isOpened = true;
-        this.currentTag.position = Position.start;
-        break;
-      case Type.endTagOpen:
-        this.currentTag.isOpened = true;
-        this.currentTag.position = Position.end;
-        break;
-      case Type.startTagElement:
-        this.currentTag.element = lexeme;
-        break;
-      case Type.endTagElement:
-        this.currentTag.element = lexeme;
-        break;
-      case Type.tagClose:
-        if (
-          this.currentTag.position === Position.start &&
-          this.currentTag.isOpened
-        ) {
-          this.currentTag.isOpened = false;
-          this.stack.push(this.currentTag);
-          this.currentTag = null;
-          break;
-        }
-        this.currentTag.isOpened = false;
-        this.stack.push(this.currentTag);
-        this.currentTag = null;
-        break;
-      case Type.attributeKey:
-        const attribute = {
-          key: lastLexeme,
-          value: null,
-          isOpened: true,
-        };
-        this.currentTag.attributes.push(attribute);
-        break;
-      case Type.attributeValue:
-        Object.values(this.currentTag.attributes).forEach((attribute, i) => {
-          if (attribute.isOpened) {
-            this.currentTag.attributes[i].value = lexeme;
-            this.currentTag.attributes[i].isOpened = false;
-          }
-        });
-        break;
-      case Type.text:
-        this.stack.push(new Text(lexeme));
-        break;
-      default:
-        console.error('Error', lexeme);
-        break;
-    }
-  }
-
-  addNodeToTree() {
-    let depth = 0;
-    let i = this.stack.getLength();
-
-    while (i > 0) {
-      const tag = this.stack.pop();
-      const node = new Node({
-        element: tag.element,
-        attributes: tag.attributes,
-        children: null,
-        depth: depth,
-      });
-
-      const isStartTag = tag.position === Position.start;
-
-      if (isStartTag) {
-        if (this.stack.searchCloseTag(tag.element)) {
-          this.tree.addChild(node, depth);
-          depth++;
-          return true;
-        }
-        return false;
-      }
-    }
-    i--;
-  }
-
   parse(lexemeContiner) {
+    let segmentContainer = [];
+    let segment = [];
     lexemeContiner.forEach((lexeme, i) => {
-      const lastLexeme = i !== 0 ? lexemeContiner[i - 1] : null;
-      const nextLexeme = lexeme.length !== i + 1 ? lexemeContiner[i + 1] : null;
-      const type = this.checkType({ lastLexeme, lexeme, nextLexeme });
-      if (type) {
-        // console.log(Object.keys(Type).find((v) => Type[v] === type));
-        this.tagFactory(type, { lastLexeme, lexeme, nextLexeme });
+      const nextLexeme = lexemeContiner[i + 1];
+      if (nextLexeme === 'LeftArrowBracker') {
+        segment.push(lexeme);
+        segmentContainer.push(segment);
+        segment = [];
+        return;
       }
-      // console.log(this.stack);
+      return segment.push(lexeme);
     });
+    if (!this.tagFactory(segmentContainer)) return 'Error: Invalid tag';
+    console.log(this.stack);
+  }
 
-    if (!this.addNodeToTree()) return "Error: Can't find the close tag";
+  tagFactory(segmentContainer) {
+    let tag = new Tag();
+    segmentContainer.forEach((segment, i) => {
+      const position = segment.includes(TOKENS.get('/'))
+        ? Position.end
+        : Position.start;
+      tag.position = position;
 
-    console.log(this.tree);
-    return this.tree;
+      const startTag = segment[0];
+      const endTagIndex = this.findCloseTagIndex(segment);
+      const endTag = segment[endTagIndex];
+      const isTagValid = this.checkTagType(startTag, endTag);
+      if (!isTagValid) return false;
+      tag.type = startTag;
+
+      if (tag.position === Position.end) {
+        this.stack.push(tag);
+        tag = new Tag();
+        return true;
+      }
+
+      const element = segment[1];
+      tag.element = element;
+
+      if (endTagIndex !== segment.length - 1) {
+        const text = segment.slice(endTagIndex + 1, segment.length);
+        tag.text = text;
+      }
+
+      const remains = segment.slice(2, endTagIndex);
+      const attributes = [];
+      remains.forEach((lexeme, i) => {
+        if (lexeme === TOKENS.get('=')) {
+          const key = remains[i - 1];
+          const value = remains[i + 1];
+          attributes.push({ key, value });
+        }
+      });
+      if (attributes.length) tag.attributes = attributes;
+
+      this.stack.push(tag);
+      tag = new Tag();
+    });
+    return true;
+  }
+
+  findCloseTagIndex(segment) {
+    let result = null;
+    segment.forEach((lexeme, i) => {
+      if (lexeme === TOKENS.get('>') || lexeme === TOKENS.get(']'))
+        return (result = i);
+    });
+    return result;
+  }
+
+  checkTagType(startTag, endTag) {
+    const [startTagType, endTagType] = [startTag, endTag].map((lexeme) => {
+      if (lexeme === TOKENS.get('<') || lexeme === TOKENS.get('>')) {
+        return TAG_TYPE.arrowBracker;
+      }
+      if (lexeme === TOKENS.get('[') || lexeme === TOKENS.get(']')) {
+        return TAG_TYPE.squareBarcker;
+      }
+    });
+    return startTagType === endTagType;
   }
 }
 
+const TAG_TYPE = {
+  arrowBracker: 1,
+  squareBarcker: 2,
+};
+
 const data1 = `<price unit="dallor" type="low"><test>ddd</test><body>29.99</body></price>`;
-const data2 = `<price><BODY>29.99</BODY></price>`;
-const data3 = `[1, [2,[3]],'hello', 'world', null]`;
-const data4 = `[ 23, “JK”, false ]`;
-const error1 = `<HTML lang="ko"></BODY>`;
+const data2 = `<price unit="dallor" type="low">/<test>ddd/</test><body>29.99</body></price>`;
 
 const lexemeContiner = new LexerAnalyer().input(data1);
 const parser = new Parser();
