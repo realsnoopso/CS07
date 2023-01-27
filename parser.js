a5d7edb4cfde3c17d42a6ec5b4ae62da063afbd2
snoopso <real@snoop.so>
2023-01-27 11:30:56 +0900

    Add a lastToken property to Parser

--- parser.js
+++ parser.js
@@ -39,70 +39,87 @@ class Tag {
 class Parser {
   constructor() {
     this.stack = new Stack();
+    this.lastToken = null;
+  }
+
+  generateTag(lexeme) {
+    const tag = new Tag();
+    tag.isStartTagOpened = true;
+    tag.element = lexeme;
+    this.stack.push(tag);
   }
 
   parse(lexemeContiner) {
-    let lastToken = null;
     const stack = this.stack;
     let currentTag = null;
     lexemeContiner.forEach((lexeme) => {
-      if (!lastToken) return (lastToken = lexeme);
-      const isElement = lastToken === 'LeftArrowBracker';
-      if (isElement) {
-        if (lexeme !== 'Div') {
-          const tag = new Tag();
-          tag.isStartTagOpened = true;
-          tag.element = lexeme;
-          stack.push(tag);
-          currentTag = stack.getLast();
-          lastToken = lexeme;
-          return;
-        }
-        const tag = stack.getLast();
-        if (tag.isCloseTagOpened) tag.isCloseTagOpened = false;
+      if (!this.lastToken) return (this.lastToken = lexeme);
+
+      const isFirstElement =
+        this.lastToken === 'LeftArrowBracker' && lexeme !== 'Div';
+      if (isFirstElement) {
+        this.generateTag(lexeme);
+        currentTag = stack.getLast();
+        currentTag.startToken = this.lastToken;
+        this.lastToken = lexeme;
         return;
       }
+
+      const isLastElement = this.lastToken === 'Div';
+      if (isLastElement) {
+        const tag = stack.getLast();
+        if (tag.isCloseTagOpened) tag.isCloseTagOpened = false;
+        this.lastToken = lexeme;
+        return;
+      }
+
       switch (lexeme) {
         case 'LeftArrowBracker':
-          lastToken = lexeme;
+          this.lastToken = lexeme;
           break;
         case 'RightArrowBracker':
-          if (currentTag.isStartTagOpened) {
+          const isStartTag = currentTag.isStartTagOpened;
+          if (isStartTag) {
             currentTag.isStartTagOpened = false;
+            this.lastToken = lexeme;
             break;
           }
-          if (currentTag.isCloseTagOpened) {
+          const isCloseTag = currentTag.isCloseTagOpened;
+          if (isCloseTag) {
             currentTag.isCloseTagOpened = false;
+            this.lastToken = null;
             break;
           }
         case 'Equal':
           const attribute = {
-            key: lastToken,
+            key: this.lastToken,
             value: null,
             isOpened: true,
           };
           currentTag.attributes.push(attribute);
+          this.lastToken = lexeme;
           break;
         case 'Div':
-          if (lastToken === 'LeftArrowBracker')
+          if (this.lastToken === 'LeftArrowBracker')
             currentTag.isCloseTagOpened = true;
+          this.lastToken = lexeme;
           break;
         default:
           const isAttributeValue = currentTag.isStartTagOpened;
-          const isText = !currentTag.isStartTagOpened;
-          const isCloseElement = lastToken === 'Div';
-
           if (isAttributeValue) {
             Object.values(currentTag.attributes).forEach((attribute) => {
               if (attribute.isOpened) attribute.value = lexeme;
               attribute.isOpened = false;
+              this.lastToken = lexeme;
             });
           }
+
+          const isText = !currentTag.isStartTagOpened;
           if (isText) {
             currentTag.text = lexeme;
+            this.lastToken = lexeme;
           }
       }
-      lastToken = lexeme;
     });
     console.log({ stack: this.stack.arr });
     return this.stack;
@@ -122,11 +139,16 @@ class Parser {
 // - 'price': 'lastToken'이 '/'임을 확인, 'isOpened.closedTag = true' 임을 확인 후 'element'를 대조
 // - '>': 'isOpened.closedTag = false'
 
-const data1 = `<price unit="dallor" type="low">29.99</price>`;
-// const data2 = `<price><BODY>29.99</BODY></price>`;
-// const data3 = `[1, [2,[3]],'hello', 'world', null]`;
-// const data4 = `[ 23, “JK”, false ]`;
-// const error1 = `<HTML lang="ko"></BODY>`;
+const data1 = `<price unit="dallor" type="low"><body>29.99</body></price>`;
+const data2 = `<price><BODY>29.99</BODY></price>`;
+const data3 = `[1, [2,[3]],'hello', 'world', null]`;
+const data4 = `[ 23, “JK”, false ]`;
+const error1 = `<HTML lang="ko"></BODY>`;
 
-const lexemeContiner = new LexerAnalyer().input(data1);
-new Parser().parse(lexemeContiner);
+const lexemeContiner = new LexerAnalyer().input(data3);
+// new Parser().parse(lexemeContiner);
+
+// 코드 정리
+// text가 다음 tag로 가는 문제 발생하긴 함
+// [, ] 이거도 대응해야 함
+// error 대응해야 함
